8aa143b6ac6c894099e1e8e52f33598021c4a83a
Shane Hingtgen <shanehingtgen@gmail.com>
2024-01-20 19:54:06 -0600

    backing up

--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,5 +1,14 @@
-export default function TabButton({children}) {
-    return (
-        <li><button>{children}</button></li>
-    )
+export default function TabButton({ children, onSelect }) {
+  // how to do it in javascript:
+  // document.querySelector('button').addEventListener('click', () => {})
+  //how to do it in react:
+  //   function handleClick() {
+  //     console.log("Hello World!");
+  //   }
+
+  return (
+    <li>
+      <button onClick={onSelect}>{children}</button>
+    </li>
+  );
 }
